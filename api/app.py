68f6290268c13878adc092a7ca155fedccc844b9
teylorguaitolini <tmg@DESKTOP-F59B8D6>
2024-07-26 19:43:23 -0300

    Adding the API

---
+++ api/app.py
@@ -0,0 +1,43 @@
+from flask import Flask, request, jsonify
+import mysql.connector
+from mysql.connector import Error
+
+app = Flask(__name__)
+
+def create_connection():
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host="car_database",
+            user="user",
+            password="userpassword",
+            database="cars_db"
+        )
+    except Error as e:
+        print(f"The error '{e}' occurred")
+    return connection
+
+@app.route('/cars', methods=['GET'])
+def get_cars():
+    connection = create_connection()
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM cars")
+    result = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    return jsonify(result)
+
+@app.route('/cars', methods=['POST'])
+def add_car():
+    new_car = request.json
+    connection = create_connection()
+    cursor = connection.cursor()
+    query = "INSERT INTO cars (make, model, year) VALUES (%s, %s, %s)"
+    cursor.execute(query, (new_car['make'], new_car['model'], new_car['year']))
+    connection.commit()
+    cursor.close()
+    connection.close()
+    return jsonify({"message": "Car added successfully!"}), 201
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
